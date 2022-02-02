b64e7b93a109b1941b55fc349434d78459e5a72e
reama623 <reama623@gmail.com>
2022-02-02 21:48:41 +0900

    public and private keyword of class property.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(name: string) {
     this.name = name;
@@ -9,13 +10,32 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("accounting");
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+// avoid this case. because.. your co-worker don't know this case(add employee method).
+// employees property is 'private'
+// accounting.employees[2] = "Anna";
+
+// name property is 'public'
+accounting.name = "NEW NAME";
 accounting.describe();
+accounting.printEmployeeInformation();
 
 // solution2. add property name of accountingCopy
-const accountingCopy = { name: "dummy", describe: accounting.describe };
+// const accountingCopy = { name: "dummy", describe: accounting.describe };
 // this.name >> undefined. Because this.name's this is accountingCopy.
 // accountingCopy hasn't name property.
-accountingCopy.describe();
+// accountingCopy.describe();
