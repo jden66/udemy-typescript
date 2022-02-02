b97093c49cfd7b4c0378b1f09c6cefcfa43d501e
reama623 <reama623@gmail.com>
2022-02-02 21:37:25 +0900

    chapter 61
    * constructor function and keyword 'this'

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,19 @@
 "use strict";
-class Department {
-    constructor(name) {
+var Department = /** @class */ (function () {
+    function Department(name) {
         this.name = name;
     }
-}
-const accounting = new Department('accounting');
-console.log(accounting);
+    // solution1. add parameter 'this' of Department type.
+    Department.prototype.describe = function () {
+        console.log("Department: " + this.name);
+    };
+    return Department;
+}());
+var accounting = new Department("accounting");
+accounting.describe();
+// solution2. add property name of accountingCopy
+var accountingCopy = { name: "dummy", describe: accounting.describe };
+// this.name >> undefined. Because this.name's this is accountingCopy.
+// accountingCopy hasn't name property.
+accountingCopy.describe();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,18 @@ class Department {
   constructor(name: string) {
     this.name = name;
   }
+
+  // solution1. add parameter 'this' of Department type.
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
 }
 
-const accounting = new Department('accounting')
+const accounting = new Department("accounting");
+accounting.describe();
 
-console.log(accounting)
+// solution2. add property name of accountingCopy
+const accountingCopy = { name: "dummy", describe: accounting.describe };
+// this.name >> undefined. Because this.name's this is accountingCopy.
+// accountingCopy hasn't name property.
+accountingCopy.describe();
