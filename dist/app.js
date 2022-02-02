63c18b15a04968751e1d012218f9c05bd533cb03
reama623 <reama623@gmail.com>
2022-02-02 20:51:01 +0900

    initial commit
    * initial commit
    * making first class

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+class Department {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const accounting = new Department('accounting');
+console.log(accounting);
+//# sourceMappingURL=app.js.map
