3249bd52281d1a08c2d367e87f059f735c74b6ef
reama623 <reama623@gmail.com>
2022-02-05 00:41:21 +0900

    73. use case class and interface

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,23 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
+class Person implements Greetable {
+  name: string;
+  age = 30;
+  constructor(n: string) {
+    this.name = n;
+  }
+  greet(phrase: string): void {
+    console.log(phrase + " " + this.name);
+  }
+}
+
 // type is interface type.
-let user1: Person;
+let user1: Greetable;
 
 // user1 variable has to Person interface defined property.
-user1 = {
-  name: "Max",
-  age: 10,
-  greet(phrase) {
-    console.log(phrase + " " + this.name);
-  },
-};
-
+user1 = new Person("Max");
 user1.greet("hello...!!");
