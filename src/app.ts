5bb00ae0488fd9ec8a4dcd497fad30124a91776b
reama623 <reama623@gmail.com>
2022-02-06 18:24:45 +0900

    84. add information of type guard.

--- src/app.ts
+++ src/app.ts
@@ -46,3 +46,55 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric; // & operator. common type is number.
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("name: " + emp.name);
+  // console.log("privileges: " + emp.privileges); // error
+  if ("privileges" in emp) {
+    console.log("privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("start date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log("drive...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("drive...");
+  }
+  loadCargo(cargo: string) {
+    console.log("drive the cargo...");
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // vehicle.loadCargo('something') // wrong
+  // first. use if's 'in' keyword
+  // if ("loadCargo" in vehicle) {
+  //   vehicle.loadCargo('something');
+  // }
+
+  // second. use instanceof
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo("something");
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
