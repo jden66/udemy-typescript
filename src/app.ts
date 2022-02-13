248d52215f52b7d83c037a2d1d9ce33904b2dfa3
reama623 <reama623@gmail.com>
2022-02-13 23:09:02 +0900

    97. other generic function

--- src/app.ts
+++ src/app.ts
@@ -38,3 +38,22 @@ const mergeObj = merge({ name: "abc", hobbies: ["sports"] }, { age: 30 });
 // case2. access age propergy
 // case3. occur error access age property. because merge func return obj is don't know age property..
 console.log(mergeObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+// 일반 제네릭 함수이지만, 제네릭 타입을 주면서, 매개변수의 length타입을 필수적으로 검사하게 한다.
+// 이를 통해, 매개변수를 감시할 수 있는데, 적어도 element로 들어오는 매개변수에는 length라는 프로퍼티가 필수적으로 존재해야 함을 사전에 확인할 수 있다.
+// 반환 타입까지 명시해주면, 제일 베스트 하게 함수를 완성할 수 있다.
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let elementDescription = "Got no value.";
+  if (element.length === 1) {
+    elementDescription = `Got 1 value.`;
+  } else if (element.length > 1) {
+    elementDescription = `Got ${element.length} value.`;
+  }
+  return [element, elementDescription];
+}
+
+console.log(countAndDescribe("abcd"));
